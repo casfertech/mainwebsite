53d4439fdecdeec9b155db04279b31b386d52837
pdias-cci <presley.dias@creativecapsule.com>
2017-02-20 21:46:23 +0530

    slider

--- freightrates.php
+++ freightrates.php
@@ -62,8 +62,33 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
-                    <img src="images/portfolio-details/freightrates.jpg" class="img-responsive" alt="">
+                  <br>
+                  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                      <li data-target="#myCarousel" data-slide-to="1"></li>
+                      <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                      <div class="item active">
+                        <img src="images/portfolio-details/freightrates-road.jpg" alt="Chania" width="460" height="345">
+                      </div>
+
+                      <div class="item">
+                        <img src="images/portfolio-details/freightrates-sea.jpg" alt="Chania" width="460" height="345">
+                      </div>
+                    
+                      <div class="item">
+                        <img src="images/portfolio-details/freightrates-air.jpg" alt="Flower" width="460" height="345">
+                      </div>
+                    </div>
+
+                  </div>
                 </div>
+
                 <div class="col-sm-6">
                     <div class="project-name overflow">
                         <h2 class="bold">FreightRates</h2>
